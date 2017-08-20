e3a89380628f1f88f026bc452eb76e0ffa2e3046
Andriy Knysh <aknysh@users.noreply.github.com>
2017-08-20 16:13:57 -0400

    Pin `tf_label` and `tf_hostname` to 0.1.0 (#4)

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Define composite variables for resources
 module "label" {
-  source    = "git::https://github.com/cloudposse/tf_label.git"
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
   namespace = "${var.namespace}"
   name      = "${var.name}"
   stage     = "${var.stage}"
@@ -109,7 +109,7 @@ resource "aws_instance" "default" {
 }
 
 module "dns" {
-  source    = "git::https://github.com/cloudposse/tf_hostname.git"
+  source    = "git::https://github.com/cloudposse/tf_hostname.git?ref=tags/0.1.0"
   namespace = "${var.namespace}"
   name      = "${var.name}"
   stage     = "${var.stage}"
