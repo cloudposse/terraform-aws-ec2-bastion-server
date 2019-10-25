2c2db6de5d2308c01789ff5873f833ebf0907612
Valeriy Onishchenko <drama17@gmail.com>
2019-10-25 14:20:40 +0300

    Update manifests to be consistent with terraform >= 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,30 @@
 output "instance_id" {
-  value       = "${aws_instance.default.id}"
+  value       = aws_instance.default.id
   description = "Instance ID"
 }
 
 output "ssh_user" {
-  value       = "${var.ssh_user}"
+  value       = var.ssh_user
   description = "SSH user"
 }
 
 output "security_group_id" {
-  value       = "${aws_security_group.default.id}"
+  value       = aws_security_group.default.id
   description = "Security group ID"
 }
 
 output "role" {
-  value       = "${aws_iam_role.default.name}"
+  value       = aws_iam_role.default.name
   description = "Name of AWS IAM Role associated with the instance"
 }
 
 output "public_ip" {
-  value       = "${aws_instance.default.public_ip}"
+  value       = aws_instance.default.public_ip
   description = "Public IP of the instance (or EIP)"
 }
 
 output "private_ip" {
-  value       = "${aws_instance.default.private_ip}"
+  value       = aws_instance.default.private_ip
   description = "Private IP of the instance"
 }
+
