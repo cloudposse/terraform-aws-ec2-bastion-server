76c95845367e37876e57604b6005f9d31c9d1a37
Tyrone Meijn <t.meijn@thesio.eu>
2020-09-24 08:57:10 +0200

    Fix format of outputs
    What
    
    * Fix format of outputs so no trailing 0 is needed
    
    Why
    
    * Currently I have to suffix the `instance_id` with a 0: module.bastion.instance_id[0]. This PR will fix this.

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "instance_id" {
-  value       = aws_instance.default.*.id
+  value       = join("", aws_instance.default.*.id)
   description = "Instance ID"
 }
 
@@ -9,22 +9,22 @@ output "ssh_user" {
 }
 
 output "security_group_id" {
-  value       = aws_security_group.default.*.id
+  value       = join("", aws_security_group.default.*.id)
   description = "Security group ID"
 }
 
 output "role" {
-  value       = aws_iam_role.default.*.name
+  value       = join("", aws_iam_role.default.*.name)
   description = "Name of AWS IAM Role associated with the instance"
 }
 
 output "public_ip" {
-  value       = aws_instance.default.*.public_ip
+  value       = join("", aws_instance.default.*.public_ip)
   description = "Public IP of the instance (or EIP)"
 }
 
 output "private_ip" {
-  value       = aws_instance.default.*.private_ip
+  value       = join("", aws_instance.default.*.private_ip)
   description = "Private IP of the instance"
 }
 
