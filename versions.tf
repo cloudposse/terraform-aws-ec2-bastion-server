bf4c2b8559fb58872f0e375b1924499b7411bbb9
Maxime Brunet <maxime.brunet@paytm.com>
2020-12-04 19:08:18 -0800

    Support AWS provider v3

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 0.12.0"
 
   required_providers {
-    aws      = "~> 2.55"
+    aws      = ">= 2.55, <4.0"
     null     = "~> 2.1"
     template = "~> 2.1"
   }
