1f6e950f029c61d01bbca63c955ddffc6fc47158
Maxim Mironenko <maxim@cloudposse.com>
2020-12-23 15:20:03 +0700

    example submodules upgraded, test dependencies reduced

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "cloudposse/vpc/aws"
-  version = "0.17.0"
+  version = "0.18.1"
 
   cidr_block = "172.16.0.0/16"
 
@@ -13,25 +13,25 @@ module "vpc" {
 
 module "subnets" {
   source               = "cloudposse/dynamic-subnets/aws"
-  version              = "0.30.0"
+  version              = "0.33.0"
   availability_zones   = var.availability_zones
-  context              = module.this.context
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
   nat_gateway_enabled  = false
   nat_instance_enabled = false
+
+  context              = module.this.context
 }
 
 module "aws_key_pair" {
   source              = "cloudposse/key-pair/aws"
-  version             = "0.14.0"
-  namespace           = module.this.namespace
-  stage               = module.this.stage
-  name                = module.this.name
+  version             = "0.16.1"
   attributes          = ["ssh", "key"]
   ssh_public_key_path = var.ssh_key_path
   generate_ssh_key    = var.generate_ssh_key
+
+  context              = module.this.context
 }
 
 module "ec2_bastion" {
@@ -42,8 +42,6 @@ module "ec2_bastion" {
   ami           = var.ami
   instance_type = var.instance_type
 
-  context = module.this.context
-
   security_groups         = compact(concat([module.vpc.vpc_default_security_group_id], var.security_groups))
   ingress_security_groups = var.ingress_security_groups
   subnets                 = module.subnets.public_subnet_ids
@@ -53,4 +51,6 @@ module "ec2_bastion" {
   user_data = var.user_data
 
   vpc_id = module.vpc.vpc_id
+
+  context = module.this.context
 }
