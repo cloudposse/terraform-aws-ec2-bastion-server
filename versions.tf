7fb2f58dec47d63a748b9dd15ab26e8032667e4f
Maxim Mironenko <maxim@cloudposse.com>
2021-01-20 20:19:48 +0700

    template provide reverted

--- versions.tf
+++ versions.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_version = ">= 0.12.26"
 
@@ -6,6 +5,10 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 2.55"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.1"
     }
   }
 }
