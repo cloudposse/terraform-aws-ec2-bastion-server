71f460537774ee2427630a51c6179ad2d974865d
Callum <callumccr@gmail.com>
2022-02-04 15:11:47 +0000

    fix: removed conenct logic from user_data

--- user_data/amazon-linux.sh
+++ user_data/amazon-linux.sh
@@ -4,7 +4,7 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 ##
 ## Setup SSH Config
 ##
-cat <<"__EOF__" > /home/${ssh_user}/.ssh/config
+cat <<"__EOF__" >/home/${ssh_user}/.ssh/config
 Host *
     StrictHostKeyChecking no
 __EOF__
@@ -14,21 +14,15 @@ chown ${ssh_user}:${ssh_user} /home/${ssh_user}/.ssh/config
 ##
 ## Enable SSM
 ##
-if [  "${ssm_enabled}" = "true" ]
-then
+if [ "${ssm_enabled}" = "true" ]; then
     systemctl enable amazon-ssm-agent
     systemctl start amazon-ssm-agent
     systemctl status amazon-ssm-agent
+
 else
     systemctl disable amazon-ssm-agent
     systemctl stop amazon-ssm-agent
     systemctl status amazon-ssm-agent
-fi
-
-if [  "${ec2_instance_connect_enabled}" = "true" ]
-then
-    sudo yum install ec2-instance-connect
-    sudo less /etc/ssh/sshd_config
 fi
 
 ${user_data}
