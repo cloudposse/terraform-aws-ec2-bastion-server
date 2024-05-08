f245a05d5fd1c8d9b2cd8c20f62902056ea20c30
nitrocode <7775707+nitrocode@users.noreply.github.com>
2024-05-08 00:01:51 -0500

    ci: update test modules

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,20 +4,21 @@ provider "aws" {
 
 module "vpc" {
   source  = "cloudposse/vpc/aws"
-  version = "0.25.0"
+  version = "2.1.1"
 
-  cidr_block = "172.16.0.0/16"
+  ipv4_primary_cidr_block = "172.16.0.0/16"
 
   context = module.this.context
 }
 
 module "subnets" {
   source               = "cloudposse/dynamic-subnets/aws"
-  version              = "0.39.3"
+  version              = "2.4.2"
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
+  ipv4_enabled         = true
+  ipv4_cidr_block      = module.vpc.vpc_cidr_block
   nat_gateway_enabled  = false
   nat_instance_enabled = false
 
